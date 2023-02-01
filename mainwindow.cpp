3c0d1537c990f48dc4dedeb60594a901c5a626c1
carthy27 <98388152+carthy27@users.noreply.github.com>
2023-02-01 17:43:50 +0000

    Displays a text box after its clicked.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,13 +3,14 @@
 #include <QtCore>
 #include <QtGui>
 #include <QMessageBox>
-
+#include<QCheckBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->checkBox->setChecked(true);
 }
 
 MainWindow::~MainWindow()
@@ -20,5 +21,17 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     QMessageBox::information(this,"Dinner","This is whats for dinner");
+}
+
+void MainWindow::on_checkBox_clicked()
+{
+    if(ui->checkBox->isChecked()){
+        QMessageBox::information(this,"Milk","You have milk");
+    }
+    else{
+        QMessageBox::information(this,"Milk","You dont have milk");
+
+    }
+
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -19,6 +19,8 @@ private slots:
 
     void on_pushButton_clicked();
 
+    void on_checkBox_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
